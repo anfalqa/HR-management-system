0f68458c6b5de4bbd7eb30374712f72af9ab48fc
Anfal Abdallah Aldraidi <131157224+anfalqa@users.noreply.github.com>
2023-05-29 13:47:20 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
-
+let id=1000;
+const allemp = [];
 
 function Employees(employeeName,employeeDep,employeeLevel,img){
     this.idUnique=0,
@@ -13,18 +14,18 @@ function Employees(employeeName,employeeDep,employeeLevel,img){
     this.calculatingSalary =function(){
         if (this.level==="Senior") {
             let max=2000;
-            let nim=1500;
-            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
+            let min=1500;
+            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
             return Math.round(sal-(sal*0.075));
         } else if (this.level==="Mid-Senior") {
             let max=1500;
             let nim=1000;
-            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
+            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
             return Math.round(sal-(sal*0.075));
         } else {
             let max=1000;
             let nim=500;
-            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
+            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
             return Math.round(sal-(sal*0.075));
         }
    },
@@ -94,7 +95,7 @@ let div1=document.createElement("div");
     info.textContent=(str);
     div1.appendChild(info);
     
-
+//////if part 
     if (this.dep==="Administration"){
         divA.appendChild(div1);
     } else if (this.dep==="Finance"){
@@ -141,6 +142,4 @@ for (let i = 0; i < allemp.length; i++) {
     allemp[i].generatingEmployeeID();
     allemp[i].renderEmp();
   }
-
-
 
