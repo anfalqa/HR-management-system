376157791497c132e4f0067a230d4ac0383dab86
Anfal Abdallah Aldraidi <131157224+anfalqa@users.noreply.github.com>
2023-05-29 13:59:23 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,18 +14,18 @@ function Employees(employeeName,employeeDep,employeeLevel,img){
     this.calculatingSalary =function(){
         if (this.level==="Senior") {
             let max=2000;
-            let min=1500;
-            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
+            let nim=1500;
+            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
             return Math.round(sal-(sal*0.075));
         } else if (this.level==="Mid-Senior") {
             let max=1500;
             let nim=1000;
-            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
+            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
             return Math.round(sal-(sal*0.075));
         } else {
             let max=1000;
             let nim=500;
-            let sal=  Math.floor(Math.random() * (max - min + 1) + min)
+            let sal=  Math.floor(Math.random() * (max - nim + 1) + nim)
             return Math.round(sal-(sal*0.075));
         }
    },
@@ -95,7 +95,7 @@ let div1=document.createElement("div");
     info.textContent=(str);
     div1.appendChild(info);
     
-//////if part 
+
     if (this.dep==="Administration"){
         divA.appendChild(div1);
     } else if (this.dep==="Finance"){
@@ -127,8 +127,6 @@ div1.style.height="300px";
 };
 
 
-
-
 let employee0=new Employees('Ghazi Samer','Administration','Senior',"./assets/Ghazi.jpg");
 let employee1=new Employees('Lana Ali','Finance','Senior',"./assets/Lana.jpg");
 let employee2=new Employees('Tamara Ayoub','Marketing','Senior',"./assets/Tamara.jpg");
@@ -142,4 +140,3 @@ for (let i = 0; i < allemp.length; i++) {
     allemp[i].generatingEmployeeID();
     allemp[i].renderEmp();
   }
-
